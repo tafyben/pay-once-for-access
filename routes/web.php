4daec2aa83894d8146ca7cc0ff6475b0b89391d2
Tafadzwa Ben <tbmukwepa@gmail.com>
2023-07-13 18:39:17 +0200

    added members route and view in the we and resources file and folder respectively

--- routes/web.php
+++ routes/web.php
@@ -28,4 +28,6 @@ Route::middleware('auth')->group(function () {
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
 });
 
+Route::get('members', \App\Http\Controllers\MemberIndexController::class);
+
 require __DIR__.'/auth.php';
