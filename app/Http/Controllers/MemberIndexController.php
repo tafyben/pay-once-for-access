dd15a7d91c1c1723963b93585209852f302b5a46
Tafadzwa Ben <tbmukwepa@gmail.com>
2023-07-14 13:04:15 +0200

    allow blocking the members area using the __construct

--- app/Http/Controllers/MemberIndexController.php
+++ app/Http/Controllers/MemberIndexController.php
@@ -2,10 +2,15 @@
 
 namespace App\Http\Controllers;
 
+use App\Http\Middleware\RedirectIfNotMember;
 use Illuminate\Http\Request;
 
 class MemberIndexController extends Controller
 {
+    public function __construct()
+    {
+        $this->middleware(RedirectIfNotMember::class);
+    }
     public function __invoke()
     {
         return view('members.index');
